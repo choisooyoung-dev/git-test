21333302a1ee3aaa32fbf555fec350ae00fdc785
choisooyoung-dev <sooyoung.choi.dev@gmail.com>
2023-09-27 17:43:35 +0900

    firebase practice

--- git_test.js
+++ git_test.js
@@ -1,1 +1,12 @@
-console.log("git test");
+function solution(my_string, num1, num2) {
+  var answer = "";
+  let str_arr = my_string.split("");
+  let switch_arr = str_arr[num1];
+  str_arr[num1] = str_arr[num2];
+  str_arr[num2] = switch_arr;
+  answer = str_arr.join("");
+
+  return answer;
+}
+
+solution("hello", 1, 2);
